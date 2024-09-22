903b9f7eb03446bb4f882b50740571498ce4093e
Vaibhav Vyas <vvyasdev@gmail.com>
2024-09-22 18:47:36 +0530

    Created InventoryServiceImplTest class with dummy methods

---
+++ InventoryManagement/src/test/java/com/example/InventoryManagement/services/InventoryServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.example.InventoryManagement.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void getItemByName() {
+    }
+
+    @Test
+    void getAllItems() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void deleteItem() {
+    }
+
+    @Test
+    void deleteItemByName() {
+    }
+
+    @Test
+    void getItemByCategory() {
+    }
+}
